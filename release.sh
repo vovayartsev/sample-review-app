950d0c410e2ddf256e5d2bae180e08bab75063f7
Vladimir Yartsev <vovayartsev@gmail.com>
2018-01-25 13:03:32 +0400

    Buildpacks magic

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker-compose is actually just a Python script
-# and apt-get installs it into a non-standard location
-export PYTHONPATH=/app/.apt/usr/lib/python2.7/dist-packages
-
-echo "RELEASE: Here is my environment:"
-env
