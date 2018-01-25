79743f2b8d03bd6509c84c0003fee4e94429541b
Vladimir Yartsev <vovayartsev@gmail.com>
2018-01-26 01:07:01 +0400

    PR example

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# This package provides dh-compose binary
-npm install -g dockhero
-
 echo "Sleeping for 120 sec to let Dockhero addon initialize itself..."
-# sleep 120
-
-echo "Deploying to Dockhero..."
-# exec dh-compose up -d
+sleep 120
